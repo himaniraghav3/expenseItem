1235500d5ee949eb9498c59db102006954b380f4
Himani Raghav (CTO) <hi20168020@wipro.com>
2021-07-01 22:06:09 +0530

    added Card component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,19 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+import Card from './Card';
 function ExpenseItem(props) {
     
 
     return (
 
-    <div>
-        <div className="expense-item">
+        <Card className="expense-item">
            <ExpenseDate date={props.date}></ExpenseDate>
-        <div className="expense-item__description">
-            <h2>{props.title}</h2>
-            <div className="expense-item__price">{props.amount}</div>
-        </div>
-        </div>
-    </div>);
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
+            </div>
+        </Card>
+    );
 }
 
 export default ExpenseItem;
